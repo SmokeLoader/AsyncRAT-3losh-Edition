823c3d8ed1f17d0a836ddb2499821b6cf7ea36c0
Smoke <157979116+SmokeLoader@users.noreply.github.com>
2024-09-17 21:00:32 +0200

    🔑 License generation 🔑

---
+++ AsyncRAT-3losh-Gen/License Generation/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace License_Generation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            LicenseKeyTextBox.Text = BinGen(UserIdTextBox.Text);
+        }
+
+        public static string BinGen(string id)
+        {
+            string[] iddParts = id.Split(new string[] { "_" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string combinedString = iddParts[1] + iddParts[1] + "3losh";
+
+            byte[] md5Hash = ComputeMD5Hash(combinedString);
+
+            byte[] key = new byte[32];
+            Array.Copy(md5Hash, 0, key, 0, 5);
+            Array.Copy(md5Hash, 0, key, 10, 5);
+
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = key;
+                rijndael.Mode = CipherMode.ECB;
+                rijndael.Padding = PaddingMode.PKCS7;
+
+                using (ICryptoTransform encryptor = rijndael.CreateEncryptor())
+                {
+                    byte[] iddBytes = SBxx(id);
+                    byte[] encryptedBytes = encryptor.TransformFinalBlock(iddBytes, 0, iddBytes.Length);
+
+                    return Convert.ToBase64String(encryptedBytes);
+                }
+            }
+        }
+        public static byte[] ComputeMD5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                return md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            }
+        }
+
+        public static byte[] SBxx(string input)
+        {
+            return Encoding.UTF8.GetBytes(input);
+        }
+    }
+}
